67dbb57b4f581e3253890f8c1c4136172032c107
𝑺 𝑨 𝑼 𝑹 𝑨 𝑩 𝑯 _ 𝑵 𝑨 𝑳 𝑬 <saurabhnalepatil@gmail.com>
2024-04-13 11:46:39 +0530

    Did a some dynamica operration on my project

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent {
+  constructor() { }
 
+  downloadResume(): void {
+    // Replace 'assets/resume.pdf' with the path to your resume file
+    const resumePath = 'assets/Saurabh_Nale_Resume.pdf';
+
+    // Create an anchor element
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', resumePath);
+    link.setAttribute('download', 'resume.pdf'); // Set the default download filename
+
+    // Simulate a click event to trigger the download
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 }
