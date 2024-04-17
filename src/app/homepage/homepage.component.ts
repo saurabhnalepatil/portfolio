35dc7894332fa74bde293f12ddff62ed214758a7
𝑺 𝑨 𝑼 𝑹 𝑨 𝑩 𝑯 _ 𝑵 𝑨 𝑳 𝑬 <saurabhnalepatil@gmail.com>
2024-04-17 18:46:27 +0530

    added the nav bar button small sreen.

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent {
+  isNavOpen: boolean = false;
   constructor() { }
 
   downloadResume(): void {
@@ -14,10 +15,23 @@ export class HomepageComponent {
     const link = document.createElement('a');
     link.setAttribute('target', '_blank');
     link.setAttribute('href', resumePath);
-    link.setAttribute('download', 'resume.pdf'); 
+    link.setAttribute('download', 'resume.pdf');
 
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+
+  }
+
+  toggleNav() {
+    this.isNavOpen = !this.isNavOpen;
+  }
+  @HostListener('document:touchstart', ['$event'])
+  closeNavOnClickOutside(event: TouchEvent) {
+    this.closeNav()
+  }
+
+  closeNav() {
+    this.isNavOpen = false;
   }
 }
