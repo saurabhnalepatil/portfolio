cc8d722de35948f89b1594ea48916aed67cf1f57
𝑺 𝑨 𝑼 𝑹 𝑨 𝑩 𝑯 _ 𝑵 𝑨 𝑳 𝑬 <saurabhnalepatil@gmail.com>
2024-04-20 10:50:27 +0530

    Upadtea my-wrok section.

--- src/app/components/my-works/my-works.component.ts
+++ src/app/components/my-works/my-works.component.ts
@@ -16,20 +16,20 @@ export class MyWorksComponent {
       link: "https://event-decoration.vercel.app"
     },
     {
-      imageUrl: "assets/images/work-1.png",
+      imageUrl: "assets/images/school.png",
       title: "School",
       likes: "09",
       comments: "2",
       description: "showcasing his expertise in Event Decoration. Explore a collection of stunning projects and experiences that demonstrate creativity and skill.",
-      link: "https://event-decoration.vercel.app"
+      link: "https://school-practice-web.vercel.app"
     },
     {
-      imageUrl: "assets/images/work-1.png",
+      imageUrl: "assets/images/food-order.png",
       title: "Food Orders",
       likes: "15",
       comments: "1",
       description: "showcasing his expertise in Event Decoration. Explore a collection of stunning projects and experiences that demonstrate creativity and skill.",
-      link: "https://event-decoration.vercel.app"
+      link: "https://food-orders-psi.vercel.app"
     },
   ];
 }
