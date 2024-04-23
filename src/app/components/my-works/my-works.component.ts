cad2fe9b8384d6465e73cef46e4b6c04bb6c0935
𝑺 𝑨 𝑼 𝑹 𝑨 𝑩 𝑯 _ 𝑵 𝑨 𝑳 𝑬 <saurabhnalepatil@gmail.com>
2024-04-23 18:47:33 +0530

    Updat the my work section.

--- src/app/components/my-works/my-works.component.ts
+++ src/app/components/my-works/my-works.component.ts
@@ -8,28 +8,45 @@ import { Component } from '@angular/core';
 export class MyWorksComponent {
   cardData = [
     {
-      imageUrl: "assets/images/work-1.png",
-      title: "Event Decorators",
-      likes: "22",
-      comments: "1",
-      description: "showcasing his expertise in Event Decoration. Explore a collection of stunning projects and experiences that demonstrate creativity and skill.",
-      link: "https://event-decoration.vercel.app"
+      "imageUrl": "assets/images/work-1.png",
+      "title": "Event Decorators",
+      "likes": "22",
+      "comments": "1",
+      "description": "Explore stunning event decorations, from weddings to corporate events, showcasing creativity and attention to detail.",
+      "link": "https://event-decoration.vercel.app"
     },
     {
-      imageUrl: "assets/images/school.png",
-      title: "School",
-      likes: "09",
-      comments: "2",
-      description: "showcasing his expertise in Event Decoration. Explore a collection of stunning projects and experiences that demonstrate creativity and skill.",
-      link: "https://school-practice-web.vercel.app"
+      "imageUrl": "assets/images/school.png",
+      "title": "School",
+      "likes": "09",
+      "comments": "2",
+      "description": "Discover innovative digital learning projects for students of all ages, focusing on engaging experiences.",
+      "link": "https://school-practice-web.vercel.app"
     },
     {
-      imageUrl: "assets/images/food-order.png",
-      title: "Food Orders",
-      likes: "15",
-      comments: "1",
-      description: "showcasing his expertise in Event Decoration. Explore a collection of stunning projects and experiences that demonstrate creativity and skill.",
-      link: "https://food-orders-psi.vercel.app"
+      "imageUrl": "assets/images/food-order.png",
+      "title": "Food Orders",
+      "likes": "15",
+      "comments": "1",
+      "description": "Experience seamless food ordering solutions and from user-friendly apps to efficient backend systems.",
+      "link": "https://food-orders-psi.vercel.app"
     },
+    {
+      "imageUrl": "assets/images/user-auth.png",
+      "title": "Auth Dashboard",
+      "likes": "13",
+      "comments": "0",
+      "description": "Manage user authentication effortlessly. Handle sign-up, sign-in, and tokens with ease. Centralized security features for seamless user management.",
+      "link": "https://auth-dashboard-frontend.vercel.app"
+    },
+    {
+      "imageUrl": "assets/images/techamica.png",
+      "title": "Software Solutions",
+      "likes": "125",
+      "comments": "5",
+      "description": "Built for Techamica, a startup software company. Emphasized simplicity and user-friendliness to reflect their commitment to accessible solutions.",
+      "link": "techamica.com"
+    }
+,
   ];
 }
